cdc3497584717a4cd6a71176a6cb3d7f46539206
Kuzmenko-Oleksandr <shparman17@gmail.com>
2023-12-16 15:30:09 +0200

    12

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".logo-container").delay(1000).fadeOut(250);
     $(".main-content").delay(1000).fadeIn(250);
+    $('.progress-container').each(function () {
+        let getMark = parseFloat($(this).find('.mark').text());
+
+        if (getMark === 12) {
+            $(this).find('.progress-fill').css('width', '100%');
+        } else {
+            let getWidth = 8.33 * getMark;
+            $(this).find('.progress-fill').css('width', getWidth + '%');
+        }
+    });
+
 });
