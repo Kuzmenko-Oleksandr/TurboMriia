83ef5c015642084f165cc4eee4bb5e4d3cb47f3b
Kuzmenko-Oleksandr <shparman17@gmail.com>
2023-12-16 17:56:16 +0200

    13

--- js/chat.js
+++ js/chat.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-
+    $(".logo-container").delay(1000).fadeOut(250);
+    $(".main-content").delay(1000).fadeIn(250);
     function formatDateTime() {
         const daysOfWeek = ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
-        const months = ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Гру'];
+        const months = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудня'];
 
         const now = new Date();
 
@@ -13,9 +14,24 @@ $(document).ready(function () {
         const hours = ('0' + now.getHours()).slice(-2);
         const minutes = ('0' + now.getMinutes()).slice(-2);
 
-        return `сьогодні ${dayOfMonth}Ж${month} ${hours}:${minutes}`;
+        return `сьогодні ${dayOfMonth} ${month} ${hours}:${minutes}`;
     }
 
     const formattedDateTime = formatDateTime();
     $('.time').text(formattedDateTime);
+
+    let clickCount = 0;
+
+    $('.chat-send').on('click', function () {
+        clickCount++;
+
+        if (clickCount ===1) {
+            $('.chat-img-2').show();
+        } else if (clickCount === 2) {
+            $('.chat-img-3').show();
+        } else if (clickCount === 3) {
+            $('.chat-img-4').show();
+        }
+
+    });
 });
